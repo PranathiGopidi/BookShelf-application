5e105452a26b853455036ba8507a1fc51f205839
PranathiGopidi <pranathireddy.1704@gmail.com>
2018-02-13 07:57:55 -0600

    commit

--- BookShelfApp/script.js
+++ BookShelfApp/script.js
@@ -1,39 +1,77 @@
-var app = angular.module('myApp', []);
-app.controller('BooksCtrl', function($scope) {
-    $scope.BooksList = [{BooksText1: 'Harry Potter and the goblet of fire', done: false,
-        BooksText2: '0-439-13959-7',
-        BooksText3: 'J.K.Rowling',
-        BooksText4: '2000',
-        BooksText5: 'http://publish.uwo.ca/~hamendt/WD%20final%20Project/litertaure/Goblet%20of%20Fire.pdf'}];
+var myApp = angular.module("myApp", ["ngRoute"]);
+myApp.config(function($routeProvider) {
+    $routeProvider
+        .when('/Home', {
+            templateUrl : "pages/Home.html",
+            controller:"BooksCtrl"
+        })
+        .when('/About', {
+            templateUrl : "pages/About.html",
+            //  controller:"AboutCtrl"
 
-    $scope.add = function () {
-        $scope.BooksList.push({BooksText1: $scope.BooksInput1,BooksText2: $scope.BooksInput2, BooksText3: $scope.BooksInput3,BooksText4: $scope.BooksInput4, BooksText5: $scope.BooksInput5,done: $scope.done});
-        $scope.BooksInput1 = "";
-        $scope.BooksInput2 = "";
-        $scope.BooksInput3 = "";
-        $scope.BooksInput4 = "";
-        $scope.BooksInput5 = "";
+        })
 
+});
+myApp.controller('BooksCtrl', function($scope) {
+    $scope.BooksList = [{BookName: 'Harry Potter and the goblet of fire', done: false,
+        ISBN: '0-439-13959-7',
+        AuthorName: 'J.K.Rowling',
+        YearPublished: '2000',
+        ReadBooks: 'http://harrypotter.scholastic.com/excerpts/HP_Book4_Chapter_Excerpt.pdf'}];
+    $scope.editIndex = false;
+    $scope.addBook = function() {
+        if( $scope.editIndex === false) {
+            $scope.BooksList.push({
+                BookName: $scope.BookName,
+                ISBN: $scope.ISBN,
+                AuthorName: $scope.AuthorName,
+                YearPublished: $scope.YearPublished,
+                ReadBooks:$scope.ReadBooks,
+                done: false
+            });
+            localStorage.setItem('BookName',JSON.stringify($scope.BookName));
+            localStorage.setItem('ISBN',JSON.stringify($scope.ISBN));
+            localStorage.setItem('AuthorName',JSON.stringify($scope.AuthorName));
+            localStorage.setItem('YearPublished',JSON.stringify($scope.YearPublished));
+            localStorage.setItem('ReadBooks',JSON.stringify($scope.ReadBooks));
+
+        }
+        else {
+            $scope.BooksList[$scope.editIndex].BookName = $scope.BookName;
+            $scope.BooksList[$scope.editIndex].ISBN = $scope.ISBN;
+            $scope.BooksList[$scope.editIndex].AuthorName = $scope.AuthorName;
+            $scope.BooksList[$scope.editIndex].YearPublished = $scope.YearPublished;
+            $scope.BooksList[$scope.editIndex].ReadBooks = $scope.ReadBooks;
+
+        }
+        $scope.editIndex = false;
+        $scope.BookName = "";
+        $scope.ISBN = "";
+        $scope.AuthorName = "";
+        $scope.YearPublished = "";
+        $scope.ReadBooks="";
 
     };
-    $scope.delete = function() {
+
+    $scope.remove = function() {
         var oldList = $scope.BooksList;
         $scope.BooksList = [];
         angular.forEach(oldList, function(x) {
             if (!x.done) $scope.BooksList.push(x);
         });
-        console.log($scope.BooksList);
     };
+    $scope.edit = function(x) {
+        $scope.BookName = $scope.BooksList[x].BookName;
+        $scope.ISBN = $scope.BooksList[x].ISBN;
+        $scope.AuthorName = $scope.BooksList[x].AuthorName;
+        $scope.YearPublished = $scope.BooksList[x].YearPublished;
+        $scope.ReadBooks = $scope.BooksList[x].ReadBooks;
 
-    var up = $scope.BooksList;
-    $scope.edit = function (x) {
-        $scope.current = x;
-        console.log($scope.current);
+        $scope.editIndex = x;
     };
-
     $scope.save = function (x) {
         $scope.current = {};
     }
-
-    $scope.current = {};
 });
+
+
